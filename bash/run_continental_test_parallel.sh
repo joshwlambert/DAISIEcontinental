c99b3f553650e6e2e723bd5aa681ec67e8f66348
joshwlambert <j.w.l.lambert@rug.nl>
2022-05-31 12:20:05 +0200

    corrected bash script

--- bash/run_continental_test_parallel.sh
+++ bash/run_continental_test_parallel.sh
@@ -4,9 +4,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=continental_test
-#SBATCH --output=/data/p287218/DAISIEprepExtra/logs/continental_test%a.log
+#SBATCH --output=/data/p287218/ContinentalTesting/logs/continental_test%a.log
 #SBATCH --array=1-216
 #SBATCH --mem=5GB
 
 ml R
-Rscript /data/p287218/DAISIEprepExtra/scripts/performance_parallel.R ${SLURM_ARRAY_TASK_ID}
+Rscript /data/p287218/ContinentalTesting/scripts/run_continental_test_parallel.R ${SLURM_ARRAY_TASK_ID}
