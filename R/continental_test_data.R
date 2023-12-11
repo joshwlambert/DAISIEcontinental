f73a1c366bdde267995036fdbae450e5398a84f9
joshwlambert <joshua.lambert@lshtm.ac.uk>
2023-12-11 16:26:53 +0000

    added continental_test_data and continental_test_sim functions

---
+++ R/continental_test_data.R
@@ -0,0 +1,71 @@
+#' Test the continental DAISIE model with empirical data
+#'
+#' @description Runs a test of the continental DAISIE model by fitting a
+#' continental DAISIE MLE model to empirical data and estimating all parameters,
+#' except the probability of initial presence which is fixed.
+#'
+#' @param island_age Duration of the island in millions of years
+#' @param num_mainland_species NUmber of species in the mainland pool that can
+#' colonise the island
+#' @param clado_rate Rate of cladogenesis (cladogenetic speciation) (per species
+#' per million years)
+#' @param ext_rate Rate of extinction (extinction rate) (per species
+#' per million years)
+#' @param carrying_cap Carrying capacity for the clade-specific diversity-
+#' dependence of cladogenesis and colonisation (immigration) rate
+#' @param immig_rate Rate of immigration (colonisation) (per species
+#' per million years)
+#' @param ana_rate Rate of anagenesis (per species per million years)
+#' @param replicates Number of replicates to run the simulation and maximum
+#' likelihood DAISIE model
+#' @param prob_init_species Probability of a species in the mainland species
+#' pool being initially on the island (i.e. vicariant species).
+#' @param prob_init_endemic Probability of a vicariant species being endemic. If
+#' a species is not endemic it is non-endemic.
+#' @param verbose Boolean determining whether to print to the console while
+#' function runs.
+#'
+#' @return A list of two elements, the first with the simulated data and the
+#' second with the maximum likelihood parameter estimates.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' example <- run_continental_test(
+#'   island_age = 1,
+#'   num_mainland_species = 100,
+#'   clado_rate = 0.5,
+#'   ext_rate = 0.1,
+#'   carrying_cap = 50,
+#'   immig_rate = 0.1,
+#'   ana_rate = 0.5,
+#'   replicates = 1,
+#'   prob_init_species = 0.5,
+#'   prob_init_endemic = 0.1,
+#'   verbose = FALSE
+#' }
+continental_test_data <- function(data,
+                                  prob_init_species,
+                                  prob_init_endemic,
+                                  verbose) {
+  browser()
+
+  mls <- list()
+  for (i in seq_len(data)) {
+
+    mls[[i]] <- DAISIE::DAISIE_ML_CS(
+      datalist = data[[i]],
+      initparsopt = c(0.1, 0.1, 200, 0.01, 0.1),
+      idparsopt = 1:5,
+      parsfix = 0.5,
+      idparsfix = 6,
+      ddmodel = 11,
+      methode = "lsodes",
+      optimmethod = "simplex"
+    )
+  }
+
+  out <- list(mls = mls, data = data)
+  class(out) <- "continental_test"
+  return(out)
+}
---
+++ R/continental_test_sim.R
@@ -0,0 +1,101 @@
+#' Test the continental DAISIE model with simulated data
+#'
+#' @description Runs a simulation test of the continental DAISIE model by
+#' simulating DAISIE data, augmenting it to represent vicariant species, and
+#' then fitting a continental DAISIE MLE model estimating all parameters,
+#' except the probability of initial presence which is fixed.
+#'
+#' @param island_age Duration of the island in millions of years
+#' @param num_mainland_species NUmber of species in the mainland pool that can
+#' colonise the island
+#' @param clado_rate Rate of cladogenesis (cladogenetic speciation) (per species
+#' per million years)
+#' @param ext_rate Rate of extinction (extinction rate) (per species
+#' per million years)
+#' @param carrying_cap Carrying capacity for the clade-specific diversity-
+#' dependence of cladogenesis and colonisation (immigration) rate
+#' @param immig_rate Rate of immigration (colonisation) (per species
+#' per million years)
+#' @param ana_rate Rate of anagenesis (per species per million years)
+#' @param replicates Number of replicates to run the simulation and maximum
+#' likelihood DAISIE model
+#' @param prob_init_species Probability of a species in the mainland species
+#' pool being initially on the island (i.e. vicariant species).
+#' @param prob_init_endemic Probability of a vicariant species being endemic. If
+#' a species is not endemic it is non-endemic.
+#' @param verbose Boolean determining whether to print to the console while
+#' function runs.
+#'
+#' @return A list of two elements, the first with the simulated data and the
+#' second with the maximum likelihood parameter estimates.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' example <- run_continental_test(
+#'   island_age = 1,
+#'   num_mainland_species = 100,
+#'   clado_rate = 0.5,
+#'   ext_rate = 0.1,
+#'   carrying_cap = 50,
+#'   immig_rate = 0.1,
+#'   ana_rate = 0.5,
+#'   replicates = 1,
+#'   prob_init_species = 0.5,
+#'   prob_init_endemic = 0.1,
+#'   verbose = FALSE
+#' }
+continental_test_sim <- function(island_age,
+                                 num_mainland_species,
+                                 clado_rate,
+                                 ext_rate,
+                                 carrying_cap,
+                                 immig_rate,
+                                 ana_rate,
+                                 replicates,
+                                 prob_init_species,
+                                 prob_init_endemic,
+                                 verbose) {
+  browser()
+  sims <- DAISIE::DAISIE_sim_cr(
+    time = island_age,
+    M = num_mainland_species,
+    pars = c(clado_rate, ext_rate, carrying_cap, immig_rate, ana_rate),
+    replicates = replicates,
+    divdepmodel = "CS",
+    nonoceanic_pars = c(prob_init_species, 1 - prob_init_endemic),
+    plot_sims = FALSE,
+    verbose = verbose
+  )
+
+  sims_precise <- sims
+
+  sims <- format_continental_data(sims)
+
+  sims_max_age <- sims
+
+  mls <- list()
+  for (i in seq_len(replicates)) {
+
+    mls[[i]] <- DAISIE::DAISIE_ML_CS(
+      datalist = sims_max_age[[i]],
+      initparsopt = c(
+        clado_rate,
+        ext_rate,
+        carrying_cap,
+        immig_rate,
+        ana_rate
+      ),
+      idparsopt = 1:5,
+      parsfix = 0.5,
+      idparsfix = 6,
+      ddmodel = 11,
+      methode = "lsodes",
+      optimmethod = "simplex"
+    )
+  }
+
+  out <- list(mls = mls, data = sims)
+  class(out) <- "continental_test"
+  return(out)
+}
