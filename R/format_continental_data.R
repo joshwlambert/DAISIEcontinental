c82584c40f913a5bbce18a17ce491854acc98e27
joshwlambert <joshua.lambert@lshtm.ac.uk>
2023-12-19 15:45:49 +0000

    modified format_continental_data function

--- R/format_continental_data.R
+++ R/format_continental_data.R
@@ -3,29 +3,29 @@
 #' @description Changes the endemicity status (stac) of the simulated DAISIE data
 #' in order to be analysed by the continental DAISIE inference model.
 #'
-#' @param sims List of DAISIE data, output by [DAISIE::DAISIE_sim_cr()].
-#' **Must have >= 1 replicate**.
+#' @param sim Single DAISIE data list (1 replicate), output by
+#' [DAISIE::DAISIE_sim_cr()].
 #'
 #' @return DAISIE data list.
 #' @keywords internal
-format_continental_data <- function(sims) {
-  for (i in seq_along(sims)) {
-    for (j in 2:length(sims[[i]])) {
+format_continental_data <- function(sim) {
+
+    for (j in 2:length(sim)) {
       vicariant_species <-
-        sims[[i]][[j]]$branching_times[1] == sims[[i]][[j]]$branching_times[2]
-      species_endemism <-  sims[[i]][[j]]$stac
-      num_clado_events <- length(sims[[i]][[j]]$branching_times) - 1
+        sim[[j]]$branching_times[1] == sim[[j]]$branching_times[2]
+      species_endemism <-  sim[[j]]$stac
+      num_clado_events <- length(sim[[j]]$branching_times) - 1
       if (vicariant_species && species_endemism == 4) {
-        sims[[i]][[j]]$stac <- 1
+        sim[[j]]$stac <- 1
       } else if (vicariant_species && species_endemism == 2 && num_clado_events >= 1) {
-        sims[[i]][[j]]$stac <- 6
+        sim[[j]]$stac <- 6
       } else if (vicariant_species && species_endemism == 2) {
-        sims[[i]][[j]]$stac <- 5
+        sim[[j]]$stac <- 5
       } else if (vicariant_species && species_endemism == 3) {
-        sims[[i]][[j]]$stac <- 7
+        sim[[j]]$stac <- 7
       }
     }
-  }
 
-  return(sims)
+
+  return(sim)
 }
