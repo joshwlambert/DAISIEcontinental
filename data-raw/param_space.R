b00860ee9564c24ca534daaf1ea4ca447929cf5b
joshwlambert <joshua.lambert@lshtm.ac.uk>
2023-12-22 11:48:56 +0000

    reduced mainland pool size, removed high clado rate and increased lower K in parameter space

--- data-raw/param_space.R
+++ data-raw/param_space.R
@@ -4,10 +4,10 @@
 
 continental_test_param_space <- expand.grid(
   total_time = list(c(5, 10, 50)),
-  m = c(500),
-  island_clado = c(0.5, 1.0),
+  m = c(250),
+  island_clado = c(0.5),
   island_ex = c(0.25, 0.5),
-  island_k = c(5, 50),
+  island_k = c(10, 50),
   island_immig = c(0.01),
   island_ana = c(0.5),
   prob_init_pres = c(0.1, 0.5, 0.9),
