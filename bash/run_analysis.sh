e4711803f1371ee89a3da5350844a5b488954825
joshwlambert <joshua.lambert@lshtm.ac.uk>
2023-12-24 16:23:06 +0000

    updated run_analysis to submit manually over array

--- bash/run_analysis.sh
+++ bash/run_analysis.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --time=0-01:00:00
+#SBATCH --time=1-23:00:00
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=analysis
 #SBATCH --output=/home3/p287218/DAISIEcontinental/logs/analysis%a.log
-#SBATCH --array=1-12
-#SBATCH --mem=1GB
+#SBATCH --array=1-10
+#SBATCH --mem=5GB
 
-sbatch /home3/p287218/DAISIEcontinental/bash/run_analysis_i.sh ${SLURM_ARRAY_TASK_ID}
+ml R
+Rscript /home3/p287218/DAISIEcontinental/scripts/run_analysis.R $1 ${SLURM_ARRAY_TASK_ID}
