a5a3a24041df4d3c002255584464eafae3cd7fff
joshwlambert <joshua.lambert@lshtm.ac.uk>
2023-12-24 17:53:11 +0000

    added boilerplate for post_process_daisie_results function

---
+++ R/post_process_daisie_results.R
@@ -0,0 +1,10 @@
+#' Compiles and saves DAISIE inference model output across the parameter space
+#'
+#' @inheritParams default_params_doc
+#'
+#' @return Void (saves file)
+#' @export
+post_process_daisie_results <- function(data_folder_path,
+                                        output_file_path) {
+
+}
