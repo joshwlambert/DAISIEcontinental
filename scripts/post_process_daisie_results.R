5918fae3b430273aa2100331b20e6d5922731d40
joshwlambert <joshua.lambert@lshtm.ac.uk>
2024-01-16 20:51:45 +0000

    added post_process_empirical_analysis to post_process_daisie_results script

--- scripts/post_process_daisie_results.R
+++ scripts/post_process_daisie_results.R
@@ -2,3 +2,8 @@ post_process_daisie_results(
   data_folder_path = file.path("inst", "pre_processed_daisie_results", "results"),
   output_file_path = file.path("inst", "post_processed_daisie_results")
 )
+
+post_process_empirical_analysis(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("inst", "post_processed_empirical_results")
+)
