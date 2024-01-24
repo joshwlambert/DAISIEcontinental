7a245dda0a0c22db3f9becc067493e96d6333573
joshwlambert <joshua.lambert@lshtm.ac.uk>
2024-01-24 07:40:29 +0000

    make install bash script a short job

--- bash/install_DAISIEcontinental.sh
+++ bash/install_DAISIEcontinental.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=1-23:00:00
+#SBATCH --time=0-23:00:00
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=install_DAISIEcontinental
 #SBATCH --output=/home3/p287218/DAISIEcontinental/install_DAISIEcontinental.log
-#SBATCH --mem=5GB
+#SBATCH --mem=2GB
 
 mkdir -p logs
 mkdir -p results
