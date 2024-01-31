4d643448d42729354e454c65611d64f9066c782f
joshwlambert <joshua.lambert@lshtm.ac.uk>
2024-01-31 07:36:55 +0000

    update file path in post_process_daisie_results script

--- scripts/post_process_daisie_results.R
+++ scripts/post_process_daisie_results.R
@@ -1,4 +1,4 @@
 post_process_daisie_results(
-  data_folder_path = file.path("inst", "pre_processed_daisie_results", "results"),
+  data_folder_path = file.path("inst", "pre_processed_daisie_results"),
   output_file_path = file.path("inst", "post_processed_daisie_results")
 )
