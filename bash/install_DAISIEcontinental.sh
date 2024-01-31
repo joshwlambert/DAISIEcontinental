8d3644b6b914774fffac7fff184a86e4cfba9aaa
joshwlambert <joshua.lambert@lshtm.ac.uk>
2024-01-31 07:36:11 +0000

    create inst/pre_processed_daisie_results in install bash script

--- bash/install_DAISIEcontinental.sh
+++ bash/install_DAISIEcontinental.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=2GB
 
 mkdir -p logs
-mkdir -p results
+mkdir -p inst/pre_processed_daisie_results
 ml R
 Rscript -e "install.packages(c('renv', 'devtools'))"
 Rscript -e "renv::restore(prompt = FALSE)"
