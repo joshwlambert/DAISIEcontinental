6124fdbf16a0ba02c4a47634cefb895a102a4ea2
joshwlambert <joshua.lambert@lshtm.ac.uk>
2024-02-04 16:26:35 +0000

    update install bash script and run_empirical_analysis script to save empirical results in inst/

--- scripts/run_empirical_analysis.R
+++ scripts/run_empirical_analysis.R
@@ -132,10 +132,11 @@ output <- list(
 
 output_name <- paste0("empirical_param_set_", param_set_idx, "_", replicate, ".rds")
 
-output_folder <- file.path("results")
+output_folder <- file.path("inst", "pre_processed_empirical_daisie_results")
 
 output_file_path <- file.path(output_folder, output_name)
 
 saveRDS(object = output, file = output_file_path)
 
 message("Finished")
+
