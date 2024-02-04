e6679e819ebca6c220c7a119c5782bbc951c567e
joshwlambert <joshua.lambert@lshtm.ac.uk>
2024-02-04 21:31:25 +0000

    added parameter plot_loglik_dist to data_vis script

--- scripts/data_vis.R
+++ scripts/data_vis.R
@@ -11,5 +11,30 @@ for (i in seq_len(18)) {
 
 DAISIEcontinental::plot_loglik_dist(
   data_folder_path = file.path("inst", "post_processed_empirical_results"),
-  output_file_path = file.path("inst", "plots", "empirical_loglik_dist.png")
+  output_file_path = file.path("inst", "plots", "empirical_clado_dist.png"),
+  parameter = "loglik"
+)
+
+DAISIEcontinental::plot_loglik_dist(
+  data_folder_path = file.path("inst", "post_processed_empirical_results"),
+  output_file_path = file.path("inst", "plots", "empirical_clado_dist.png"),
+  parameter = "lambda_c"
+)
+
+DAISIEcontinental::plot_loglik_dist(
+  data_folder_path = file.path("inst", "post_processed_empirical_results"),
+  output_file_path = file.path("inst", "plots", "empirical_ext_dist.png"),
+  parameter = "mu"
+)
+
+DAISIEcontinental::plot_loglik_dist(
+  data_folder_path = file.path("inst", "post_processed_empirical_results"),
+  output_file_path = file.path("inst", "plots", "empirical_immig_dist.png"),
+  parameter = "gamma"
+)
+
+DAISIEcontinental::plot_loglik_dist(
+  data_folder_path = file.path("inst", "post_processed_empirical_results"),
+  output_file_path = file.path("inst", "plots", "empirical_ana_dist.png"),
+  parameter = "lambda_a"
 )
