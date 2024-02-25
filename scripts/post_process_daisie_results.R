c624a1184f8503f8a021856415d37a7016379bb5
joshwlambert <joshua.lambert@lshtm.ac.uk>
2024-02-25 10:08:32 +0000

    added post_process_vicariance_results call to post_process_daisie_results script

--- scripts/post_process_daisie_results.R
+++ scripts/post_process_daisie_results.R
@@ -7,3 +7,8 @@ post_process_empirical_analysis(
   data_folder_path = file.path("inst", "pre_processed_empirical_results"),
   output_file_path = file.path("inst", "post_processed_empirical_results")
 )
+
+post_process_vicariance_results(
+  data_folder_path = file.path("inst", "pre_processed_vicariance_results"),
+  output_file_path = file.path("inst", "post_processed_vicariance_results")
+)
