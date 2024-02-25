f21504babdcb341f2a5b880e3ffd0683b7130df2
joshwlambert <joshua.lambert@lshtm.ac.uk>
2024-02-25 10:07:57 +0000

    updated install_DAISIEcontinental bash script to make pre_processed_vicariance_results dir

--- bash/install_DAISIEcontinental.sh
+++ bash/install_DAISIEcontinental.sh
@@ -10,6 +10,7 @@
 mkdir -p logs
 mkdir -p inst/pre_processed_daisie_results
 mkdir -p inst/pre_processed_empirical_results
+mkdir -p inst/pre_processed_vicariance_results
 ml R
 Rscript -e "install.packages(c('renv', 'devtools'))"
 Rscript -e "renv::restore(prompt = FALSE)"
